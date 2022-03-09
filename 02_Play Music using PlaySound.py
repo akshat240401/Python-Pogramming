8018afddd423d528f6a6800b8aaa4a1154cdd739
Akshat Mehta <akshat2404@gmail.com>
2022-03-09 16:27:50 +0530

    Practice-set-1

---
+++ 02_Play Music using PlaySound.py
@@ -0,0 +1,3 @@
+print (" !!!!!!!!!! IT'S THE BROCK LESNAR'S ENTRY !!!!!!!!!! ")
+from playsound import playsound
+playsound("C:\\Users\\aksha\Music\WWE Brock Lesnar - Next Big Thing (Entrance Theme).mp3")
