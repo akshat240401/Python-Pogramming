8857790b919a2d5852a96eab804d8cada149bd94
Akshat Mehta <akshat2404@gmail.com>
2022-07-09 11:18:18 +0530

    Lists

---
+++ 14_Lists.py
@@ -0,0 +1,13 @@
+#Create a list using []
+a = [1,"Akshat",3.0,True]
+print(a)
+
+#Access any element using index value i.e. a[0],a[1],a[2]...
+print(a[2])
+
+#Change the value using
+a[2]=100
+print(a[2])
+
+#List Slicing
+print(a[0:3])
---
+++ 15_List Methods.py
@@ -0,0 +1,24 @@
+#Sorting
+l1 = [1,8,7,2,21,15]
+l1.sort()
+print(l1)
+
+#Reversing
+l1.reverse()
+print(l1)
+
+#Append
+l1.append(45) #Appends at end
+print(l1)
+
+#Insert
+l1.insert(0,500) #Inserts at position i.e. (position,insert vlaue)
+print(l1)
+
+#Pop
+l1.pop()
+print(l1)
+
+#Remove
+l1.remove(21) #Removes a particular element
+print(l1)
