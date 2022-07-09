4be7e2eaf8d3d485df6fcbf936344b1c475bb790
Akshat Mehta <akshat2404@gmail.com>
2022-07-09 12:49:48 +0530

    Practice-set-5

---
+++ 24_Make Translate Dictionary.py
@@ -0,0 +1,5 @@
+translateDict = {"padharo":"Welcome","kaise ho":"How are you","naam":"Akshat"}
+
+print("Options Available Are: ",translateDict.keys())
+a=input("Enter the hindi word - ")
+print("The translatedd form of",a,"is- ",translateDict[a])
---
+++ 26_User Input Dictionary.py
@@ -0,0 +1,11 @@
+favLang ={}
+l1 = (input("Enter lang 1- "))
+l3 = (input("Enter lang 3- "))
+l4 = (input("Enter lang 4- "))
+l2 = (input("Enter lang 2- "))
+favLang['Akshat']= l1
+favLang['Hahith']= l2
+favLang['Rutuja']= l3
+favLang['Aayush']= l4
+
+print(favLang)
