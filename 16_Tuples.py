40bb9ec381f75f0c2e25ee7debe3269ad1b7f84e
Akshat Mehta <akshat2404@gmail.com>
2022-07-09 11:26:11 +0530

    Tuples

---
+++ 16_Tuples.py
@@ -0,0 +1,11 @@
+#Create a Tuple ()
+t = (1,"Akshat",3.0,True)
+print(t)
+
+#Access any element using index value i.e. a[0],a[1],a[2]...
+print(t[2])
+
+#Tuples are Immutable i.e. values can't be modified
+
+#Tuple Slicing
+print(t[0:3])
---
+++ 17_Tuple Methods.py
@@ -0,0 +1,7 @@
+t = (1,"Akshat",3.0,True,1)
+
+#Count
+print(t.count(3.0)) #Counts the number of times a particular value is present in tuple
+
+#Search Index
+print(t.index(3.0))
